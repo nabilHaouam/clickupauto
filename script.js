74b6a5f4ab1436db01b568996040d0a80118819d
nabilHaouam <workmailnabil@gmail.com>
2023-01-04 00:56:46 +0100

    solved the forEach error , i wasnt running the foreach function on an array, thats why it had errors, in the end , i got rid of the foreach function and replaced it with for...of function, because foreach was not designed to work with async code in the first place

--- script.js
+++ script.js
@@ -3,119 +3,128 @@ import {getTasks} from './methods/getTasks.mjs'
 import { setCustomFieldValue } from './methods/setCustomFieldValue.js';
 
 
-function toDoAutomation (){
-    const lists = getLists();
+async function toDoAutomation (){
+    const objLists = await getLists();
+    const lists = []
+    Object.entries(objLists).forEach((entry)=>{
+        const [key, value] = entry;
+        lists.push(entry)
+    })
+
     const currentDate = new Date();
     const today = currentDate.getTime()
-    lists.then( ()=>{
-        lists.forEach(list => {
-            const tasks = getTasks(list.listId);
-            tasks.then(()=>{
-                tasks.forEach(task=> {
-                    if (!task.due_date){ //if the task doesn't have a due date 
-                        if (task.value == 4){
-                            setCustomFieldValue(task.id, task.fieldId, 3)
-                        }
-                        else if (task.value == 3){
-                            setCustomFieldValue(task.id, task.fieldId, 2)
-                        }
-                        else if (task.value == 2){
-                            setCustomFieldValue(task.id, task.fieldId, 0)
-                        }
-                        else if (task.value == 0){
-                            setCustomFieldValue(task.id, task.fieldId, 9)
-                        }
-                        else if (task.value == 9){
-                            setCustomFieldValue(task.id, task.fieldId, 10)
-                        }
-                        else if (task.value == 10){
-                            setCustomFieldValue(task.id, task.fieldId, 11)
-                        }
-                        else if (task.value == 11){
-                            setCustomFieldValue(task.id, task.fieldId, 12)
-                        }
-                        else if (task.value == 12){
-                            setCustomFieldValue(task.id, task.fieldId, 13)
-                        }
-                        else if (task.value == 13){
-                            setCustomFieldValue(task.id, task.fieldId, 14)
-                        }
-                        else if (task.value == 14){
-                            setCustomFieldValue(task.id, task.fieldId, 15)
-                        }
-                        else if (task.value == 15){
-                            setCustomFieldValue(task.id, task.fieldId, 16)
-                        }
-                    }
-                    
-                    else if (task.dueDate) { //if the task has a due date 
-        
-                        const dueDate = new Date(task.dueDate);
-                        const diffTime = today - dueDate;
-                        const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24)); 
-                        
-                        if (diffDays== -3){
-                            setCustomFieldValue(task.id, task.fieldId, 19)
-                        }
-                        if (diffDays== -2){
-                            setCustomFieldValue(task.id, task.fieldId, 20)
-                        }
-                        if (diffDays== -1){
-                            setCustomFieldValue(task.id, task.fieldId, 23)
-                        }
-                        if (diffDays== 0){
-                            setCustomFieldValue(task.id, task.fieldId, 18)
-                        }
-                        if (diffDays== 1){
-                            setCustomFieldValue(task.id, task.fieldId, 21)
-                        }
-                        if (diffDays== 2){
-                            setCustomFieldValue(task.id, task.fieldId, 22)
-                        }
-                        if (diffDays== 3){
-                            setCustomFieldValue(task.id, task.fieldId, 24)
-                        }
-                        if (diffDays== 4){
-                            setCustomFieldValue(task.id, task.fieldId, 25)
-                        }
-                        if (diffDays== 5){
-                            setCustomFieldValue(task.id, task.fieldId, 26)
-                        }
-                        if (diffDays== 6){
-                            setCustomFieldValue(task.id, task.fieldId, 27)
-                        }
-                        if (diffDays== 7){
-                            setCustomFieldValue(task.id, task.fieldId, 28)
-                        }
-                        if (diffDays>7 ){
-                            setCustomFieldValue(task.id, task.fieldId, 17)
-                        }
-                    }
-                    //handling the done-->tomorrow and done-->tomorrow--auto situation
-                    if(task.value == 5){
-                        setCustomFieldValue(task.id, task.fieldId, 0)
-                    }
-                    if(task.value == 6){
-                        setCustomFieldValue(task.id, task.fieldId, 18)
-                    }
-
-                    //handling the UP NEXT situations :
-                    const diffTodayAndUpNextDate = today - task.upNextDate
-                    if (task.value == 1 && !task.upNextDate ){
-                        setCustomFieldValue(task.id, task.upNextFieldId, today.toString())
-                    }
-                    else if (diffTodayAndUpNextDate == 7){
-                        setCustomFieldValue(task.id, task.upNextFieldId, 29)
-                    }
-                    else if (diffTodayAndUpNextDate == 14){
-                        setCustomFieldValue(task.id, task.upNextFieldId, 30)
-                    }
-                    else if (diffTodayAndUpNextDate == 21){
-                        setCustomFieldValue(task.id, task.upNextFieldId, 31)
-                    }
-                })
-            })
+    for (const list of lists){
+        const objTasks = await getTasks(list[1].listId);
+        const tasks = []
+        Object.entries(objTasks).forEach((entry)=>{
+            const [key, value] = entry;
+            tasks.push(entry)
         })
-    })
+        
+        tasks.forEach(task=> {
+            if (!task[1].due_date){ //if the task[1] doesn't have a due date 
+                if (task[1].value == 4){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 3)
+                }
+                else if (task[1].value == 3){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 2)
+                }
+                else if (task[1].value == 2){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 0)
+                }
+                else if (task[1].value == 0){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 9)
+                }
+                else if (task[1].value == 9){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 10)
+                }
+                else if (task[1].value == 10){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 11)
+                }
+                else if (task[1].value == 11){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 12)
+                }
+                else if (task[1].value == 12){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 13)
+                }
+                else if (task[1].value == 13){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 14)
+                }
+                else if (task[1].value == 14){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 15)
+                }
+                else if (task[1].value == 15){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 16)
+                }
+            }
+            
+            else if (task[1].dueDate) { //if the task[1] has a due date 
+
+                const dueDate = new Date(task[1].dueDate);
+                const diffTime = today - dueDate;
+                const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24)); 
+                
+                if (diffDays== -3){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 19)
+                }
+                if (diffDays== -2){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 20)
+                }
+                if (diffDays== -1){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 23)
+                }
+                if (diffDays== 0){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 18)
+                }
+                if (diffDays== 1){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 21)
+                }
+                if (diffDays== 2){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 22)
+                }
+                if (diffDays== 3){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 24)
+                }
+                if (diffDays== 4){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 25)
+                }
+                if (diffDays== 5){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 26)
+                }
+                if (diffDays== 6){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 27)
+                }
+                if (diffDays== 7){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 28)
+                }
+                if (diffDays>7 ){
+                    setCustomFieldValue(task[1].id, task[1].fieldId, 17)
+                }
+            }
+            //handling the done-->tomorrow and done-->tomorrow--auto situation
+            if(task[1].value == 5){
+                setCustomFieldValue(task[1].id, task[1].fieldId, 0)
+            }
+            if(task[1].value == 6){
+                setCustomFieldValue(task[1].id, task[1].fieldId, 18)
+            }
+            //handling the UP NEXT situations :
+            const diffTodayAndUpNextDate = today - task[1].upNextDate
+            if (task[1].value == 1 && !task[1].upNextDate ){
+                setCustomFieldValue(task[1].id, task[1].upNextFieldId, today.toString())
+            }
+            else if (diffTodayAndUpNextDate == 7){
+                setCustomFieldValue(task[1].id, task[1].upNextFieldId, 29)
+            }
+            else if (diffTodayAndUpNextDate == 14){
+                setCustomFieldValue(task[1].id, task[1].upNextFieldId, 30)
+            }
+            else if (diffTodayAndUpNextDate == 21){
+                setCustomFieldValue(task[1].id, task[1].upNextFieldId, 31)
+            }
+        })
+    }
+   
+   
 }
 toDoAutomation();
